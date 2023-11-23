48a100984857ff20d7b2e741313a960e9d2fda3b
enchantuer <31900604+enchantuer@users.noreply.github.com>
2023-11-23 18:00:21 +0100

    Add distance calculation

--- main.ino
+++ main.ino
@@ -5,14 +5,18 @@
 #define moteur1_A 11
 #define moteur1_B 12
 
-int encodeur1_compteur = 0 ;
+#define TOTAL_NB_PAS 2400
+// #define RAYON_CODEUSE 45 // En mm
+#define RAYON_CODEUSE 35 // En mm (roue de moteur)
+#define PI 3.1415926535897932384626433832795
+
 bool encodeur1_etatA ;
 bool encodeur1_dernierEtatA ;
 
 long unsigned encodeur1_tempsA ;
 
-volatile int lastEncoded = 0;
-volatile long encoderValue = 0;
+volatile int last_encoded1 = 0;
+volatile long encoder1_value = 0;
 
 void setup() {
   // Setup reception consigne
@@ -30,6 +34,7 @@ void setup() {
 
   // attachInterrupt(digitalPinToInterrupt(encodeur1_A), encodeur1_changementA, CHANGE);
   attachInterrupt(0, updateEncoder, CHANGE); 
+  attachInterrupt(1, updateEncoder, CHANGE);
 
   // Setup moteur
   // OUTPUT
@@ -38,52 +43,25 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println(encodeur1_compteur);
-  // delay(1000); //just here to slow down the output, and show it will work  even during a delay
+  delay(1000);
+  Serial.print(calculer_distance(encoder1_value));
+  Serial.print(" | ");
+  Serial.println(encoder1_value);
 }
 
-void encodeur_changementA(){
-  // on mesure A
-  encodeur1_etatA = digitalRead(encodeur1_A);
-   
-    // controle du temps pour eviter des erreurs 
-    if( abs(millis() - encodeur1_tempsA) > 50 ){
-      // Si B different de l'ancien état de A alors
-      if(digitalRead(encodeur1_B) != encodeur1_dernierEtatA){
-        encodeur1_compteur--;
-      }
-      else{
-        encodeur1_compteur++;
-      }
-      // memorisation du temps pour A
-      encodeur1_tempsA = millis();
-    } 
-    // memorisation de l'état de A
-    encodeur1_dernierEtatA = encodeur1_etatA ;
-    
-    //affichage du compteur
-    Serial.print("Compteur 1:");
-    Serial.println(encodeur1_compteur);
-
-}
-
-void updateEncoder()
-{
+void updateEncoder() {
   int MSB = digitalRead(encodeur1_A); //MSB = most significant bit
   int LSB = digitalRead(encodeur1_B); //LSB = least significant bit
 
   int encoded = (MSB << 1) |LSB; //converting the 2 pin value to single number
-  int sum  = (lastEncoded << 2) | encoded; //adding it to the previous encoded value
+  int sum  = (last_encoded1 << 2) | encoded; //adding it to the previous encoded value
 
-  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) encoderValue ++;
-  if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) encoderValue --;
+  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) encoder1_value ++;
+  if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) encoder1_value --;
 
-  if(encoderValue == 1) {
-    encodeur1_compteur ++;
-  } else {
-    encodeur1_compteur --;
-  }
+  last_encoded1 = encoded; //store this value for next time
+}
 
-  lastEncoded = encoded; //store this value for next time
+double calculer_distance(long nb_pas) {
+  return (2*PI*RAYON_CODEUSE*nb_pas)/TOTAL_NB_PAS;
 }
